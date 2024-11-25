f6396ed8b2fc4fba9795bef5ac68cc05f41edaf0
Chris Xu <chris.xu.mps1@gmail.com>
2024-11-24 16:17:38 -0800

    Batch Correction and UMAP

--- scrna_pipeline.r
+++ scrna_pipeline.r
@@ -27,4 +27,60 @@ fscatter <- FeatureScatter(combinedSeurat,
 violin / fscatter
 ggsave("1.QC_plots.png", width = 230, units = "mm")
 
-#### Clustering
+#### Clustering and Harmony Batch Correction
+combinedSeurat <- NormalizeData(combinedSeurat,
+  normalization.method = "LogNormalize",
+  scale.factor = 10000
+)
+combinedSeurat <- FindVariableFeatures(combinedSeurat,
+  selection.method = "vst",
+  nfeatures = 2000
+)
+combinedSeurat <- ScaleData(combinedSeurat,
+  features = VariableFeatures(combinedSeurat)
+)
+# Prepare PCA for Harmony
+combinedSeurat <- RunPCA(combinedSeurat,
+  features = VariableFeatures(object = combinedSeurat),
+  verbose = FALSE, npcs = 20
+)
+combinedSeurat <- FindVariableFeatures(combinedSeurat,
+  selection.method = "vst", nfeatures = 2000
+)
+var_feat_plot <- VariableFeaturePlot(combinedSeurat) +
+  theme_bw()
+top10 <- head(x = VariableFeatures(combinedSeurat), 10)
+var_feat_plot <- LabelPoints(
+  plot = var_feat_plot, points = top10,
+  repel = TRUE, xnudge = 0, ynudge = 0
+)
+ggsave("2.Variable_Features_plot.png", width = 200, height = 150, units = "mm")
+
+# Harmony
+combinedSeurat <- RunHarmony(combinedSeurat, "orig.ident", theta = 3)
+
+# For some reason Quick-TRANSfer did not converge.
+# We check PCA plot before and after harmony batch correction.
+before_pca <- DimPlot(combinedSeurat,
+  reduction = "pca", group.by = "orig.ident",
+  pt.size = 0.1
+) +
+  ggtitle("Before Harmony") +
+  theme_minimal()
+after_pca <- DimPlot(combinedSeurat,
+  reduction = "harmony", group.by = "orig.ident",
+  pt.size = 0.1
+) +
+  ggtitle("After Harmony") +
+  theme_minimal()
+pca_plots <- before_pca + after_pca
+ggsave("3.PCA_plots_harmony.png", width = 400, height = 150, units = "mm")
+# There might not be batch effect. Tested theta = 3 for stronger correction.
+# Elbow Plot
+elbow_plot <- ElbowPlot(combinedSeurat)
+ggsave("4.Elbow_plot.png", width = 200, height = 150, units = "mm")
+# UMAP
+combinedSeurat <- RunUMAP(combinedSeurat, dims = 1:20, reduction = "harmony")
+DimPlot(combinedSeurat, reduction = "umap", split.by = "orig.ident")
+ggsave("5.UMAP_plot.png", width = 400, height = 150, units = "mm")
+saveRDS(combinedSeurat, file = "scRNA_after_umap.rds")
